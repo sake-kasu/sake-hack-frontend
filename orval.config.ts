dd590e06c9fe5df966ea681ab4e5de11c915476c
takahiro.maki <j39g8bxh@gmail.com>
2025-12-01 21:04:54 +0900

    add:🔧Orval設定を追加

---
+++ orval.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "orval";
+
+export default defineConfig({
+  api: {
+    input: "./external/backend-api/api/openapi.bundled.yaml",
+    output: {
+      mode: "single",
+      target: "./src/lib/api/generated.ts",
+      client: "axios",
+      override: {
+        mutator: {
+          path: "./src/lib/axios/client.ts",
+          name: "apiClient",
+        },
+      },
+    },
+  },
+});
