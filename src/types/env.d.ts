2501cd155a497898b2f24c2f9a8ff6051c61016f
takahiro.maki <j39g8bxh@gmail.com>
2026-02-22 14:17:35 +0900

    update:⚡未使用のOAuthランタイム設定を削除

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -1,8 +1,6 @@
 // Runtime環境変数の型定義
 export type RuntimeEnv = {
   readonly API_BASE_URL: string;
-  readonly OAUTH_SIGN_IN_URI: string;
-  readonly OAUTH_SIGN_UP_URI: string;
   readonly SESSION_KEY: string;
 };
 
